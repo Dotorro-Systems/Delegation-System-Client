46d69f2992825deed09f411e717ad14c6cfb114e
Maciej Góralczyk <goralczm@gmail.com>
2025-03-07 08:23:15 +0100

    Rename: project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Delegation_System_Client';
+  title = 'Delegation-System-Client';
 }
