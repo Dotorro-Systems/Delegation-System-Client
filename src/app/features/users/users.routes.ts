9b965e749a9f7198318536fa69eb8015cb164bbd
Maciej Góralczyk <goralczm@gmail.com>
2025-04-01 13:18:24 +0200

    Add: logout component

--- src/app/features/users/users.routes.ts
+++ src/app/features/users/users.routes.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {UserListComponent} from './components/user-list/user-list.component';
+import {LogoutComponent} from './components/logout/logout.component';
 
 const routes: Routes = [
   { path: 'list', component: UserListComponent },
+  { path: 'logout', component: LogoutComponent },
 ];
 
 @NgModule({
